46bc5a88a4fb047281cb433fe9c2b1db97689c49
Piyush <piyush1294@gmail.com>
2025-12-26 20:42:52 +0530

    Added default route table module

---
+++ Modules/default-route-table/default-route-table.tf
@@ -0,0 +1,12 @@
+resource "aws_default_route_table" "default_route_table" {
+  default_route_table_id = var.default_route_table_id
+  vpc_id = var.vpc_id
+  
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.igw-id
+  }
+  tags = {
+    name = "default-route-table"
+  }
+}
---
+++ Modules/default-route-table/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "igw-id" {
+  type = string
+}
+
+variable "default_route_table_id" {
+  type = string
+}
