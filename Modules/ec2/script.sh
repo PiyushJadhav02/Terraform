4cd6239649f015fd4fd857f41c1f2f8edafae997
Piyush <piyush1294@gmail.com>
2025-12-27 16:00:15 +0530

    Adding script.sh

--- Modules/ec2/script.sh
+++ Modules/ec2/script.sh
@@ -19,6 +19,9 @@ sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
 CNI_VERSION="v1.5.1"
 curl -LO https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-amd64-${CNI_VERSION}.tgz
 
+### JENKINS AGENT ###
+curl -sO http://13.51.64.135:8080/jnlpJars/agent.jar
+java -jar agent.jar -url http://13.51.64.135:8080/ -secret 6805549cf92f0d25722770c30735727d6428f2ee4ee4187de954527d6cc7f513 -name agent -webSocket -workDir "/home/ubuntu"
 
 usermod -aG docker ubuntu
 systemctl enable docker
@@ -38,45 +41,18 @@ install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube
 
-### FIX PERMISSIONS ###
-mkdir -p $HOME_DIR/.kube $HOME_DIR/.minikube
-cp -r /root/.kube/* $HOME_DIR/.kube/
-cp -r /root/.minikube/* $HOME_DIR/.minikube/
-chown -R ubuntu:ubuntu $HOME_DIR/.kube $HOME_DIR/.minikube
-
-### JENKINS AGENT ###
-mkdir -p /opt/jenkins
-curl -sO $JENKINS_URL/jnlpJars/agent.jar
-mv agent.jar /opt/jenkins/
-chown -R ubuntu:ubuntu /opt/jenkins
-
-cat <<EOF >/etc/systemd/system/jenkins-agent.service
-[Unit]
-Description=Jenkins Agent
-After=network.target
-
-[Service]
-User=ubuntu
-WorkingDirectory=/opt/jenkins
-ExecStart=/usr/bin/java -jar /opt/jenkins/agent.jar \
-  -url $JENKINS_URL \
-  -secret $AGENT_SECRET \
-  -name $AGENT_NAME \
-  -workDir /opt/jenkins
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reexec
-systemctl daemon-reload
-systemctl enable jenkins-agent
-systemctl start jenkins-agent
 
 ### START MINIKUBE ###
-sudo -u ubuntu minikube start \
+sudo minikube start \
   --driver=none \
   --container-runtime=containerd \
   --memory=900 \
   --force
+
+### FIX PERMISSIONS ###
+sudo chown -R $USER $HOME/.minikube
+chmod -R u+wrx $HOME/.minikube
+mkdir -p $HOME_DIR/.kube $HOME_DIR/.minikube
+cp -r /root/.kube/* $HOME_DIR/.kube/
+cp -r /root/.minikube/* $HOME_DIR/.minikube/
+chown -R ubuntu:ubuntu $HOME_DIR/.kube $HOME_DIR/.minikube
